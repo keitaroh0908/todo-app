4aa089f6e2acac65e3a9d52957b775c59494bab8
Keitaro Hayashimoto <keitaroh@hayashimoto-systems.jp>
2022-09-26 18:29:29 +0900

    feat: タスクの取得をAPI Gatewayに移動、Cognito認証を追加

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,27 +1,46 @@
 <template>
   <div id="app">
-    <h1>Todo App</h1>
-    <div class="add-task-wrapper">
-      <input type="text" v-model="newTaskInput" @keydown.enter="addTask" />
-      <button @click="addTask">Add task</button>
-    </div>
-    <div class="task" v-for="task in tasks" :key="task.id">
-      <span>{{ task.name }}</span>
-      <span class="delete">
-        <button @click="removeTask(task.id)">X</button>
-      </span>
-    </div>
+    <amplify-authenticator>
+      <div>
+        <h1>Todo App</h1>
+        <div class="add-task-wrapper">
+          <input type="text" v-model="newTaskInput" @keydown.enter="addTask" />
+          <button @click="addTask">Add task</button>
+        </div>
+        <div class="task" v-for="task in tasks" :key="task.taskId">
+          <span>{{ task.title }}</span>
+          <span class="delete">
+            <button @click="removeTask(task.id)">X</button>
+          </span>
+        </div>
+      </div>
+      <amplify-sign-out></amplify-sign-out>
+    </amplify-authenticator>
   </div>
 </template>
 
 <script>
+import { Auth } from 'aws-amplify'
+import { onAuthUIStateChange } from '@aws-amplify/ui-components'
+import axios from 'axios'
+
 export default {
   name: 'App',
   data() {
     return {
       newTaskInput: '',
-      tasks: []
+      tasks: [],
+      user: undefined,
+      authState: undefined,
+      unsubscribeAuth: undefined
     }
+  },
+  created() {
+    this.unsubscribeAuth = onAuthUIStateChange((authState, authData) => {
+      this.authState = authState
+      this.user = authData
+    })
+    this.getTasks()
   },
   methods: {
     addTask() {
@@ -34,9 +53,27 @@ export default {
       this.tasks.push(newTask)
       this.newTaskInput = ''
     },
+    getTasks: async function() {
+      Auth.currentSession()
+        .then(data => {
+          const idToken = data.getIdToken().getJwtToken()
+          axios.get('https://gl0q295fo1.execute-api.ap-northeast-1.amazonaws.com/production/tasks', {
+            headers: {
+              Authorization: idToken
+            }
+          }).then(response => {
+            this.tasks = response
+          }).catch(error => {
+            console.error(error)
+          })
+        })
+    },
     removeTask(taskId) {
       this.tasks = this.tasks.filter(task => task.id !== taskId)
     }
+  },
+  beforeDestroy() {
+    this.unsubscribeAuth()
   }
 }
 </script>
--- todo-app/src/main.js
+++ todo-app/src/main.js
@@ -1,7 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Amplify from 'aws-amplify'
+import {
+  applyPolyfills,
+  defineCustomElements
+} from '@aws-amplify/ui-components/loader'
 
 Vue.config.productionTip = false
+Vue.config.ignoredElements = [/amplify-\w*/]
+
+Amplify.configure({
+  Auth: {
+    identityPoolId: 'ap-northeast-1:87111808-c1cd-4d75-8cff-6a85646f5934',
+    region: 'ap-northeast-1',
+    identityPoolRegion: 'ap-northeast-1',
+    userPoolId: 'ap-northeast-1_BXhWUbfqi',
+    userPoolWebClientId: '1l8a1k0hvthobrnuj4l22i1skm'
+  }
+})
+
+applyPolyfills().then(() => {
+  defineCustomElements(window)
+})
 
 new Vue({
   render: h => h(App),
