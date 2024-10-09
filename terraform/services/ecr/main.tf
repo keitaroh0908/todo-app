3774aef1f1497c4e5212d42bdd4e3e76f32386e0
Keitaro Hayashimoto <keitaroh@haya-sys.com>
2024-10-09 12:07:40 +0900

    イメージのタグを変更できないよう設定する

--- terraform/services/ecr/main.tf
+++ terraform/services/ecr/main.tf
@@ -1,5 +1,6 @@
 resource "aws_ecr_repository" "this" {
-  name = "todo-app"
+  name                 = "todo-app"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
