6c6f97a52be5d6d0a6a3d67b306395eebe839b8b
Keitaro Hayashimoto <keitaroh@haya-sys.com>
2024-10-09 12:14:40 +0900

    リポジトリにライフサイクルポリシーを設定する

--- terraform/services/ecr/main.tf
+++ terraform/services/ecr/main.tf
@@ -6,3 +6,37 @@ resource "aws_ecr_repository" "this" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_lifecycle_policy" "this" {
+  repository = aws_ecr_repository.this.name
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Remove untagged images after 30 days"
+        selection = {
+          tagStatus   = "untagged"
+          countType   = "sinceImagePushed"
+          countNumber = 30
+          countUnit   = "days"
+        }
+        action = {
+          type = "expire"
+        }
+      },
+      {
+        rulePriority = 2
+        description  = "Retain only the latest 10 images for each tag"
+        selection = {
+          tagStatus      = "tagged"
+          tagPatternList = ["*"]
+          countType      = "imageCountMoreThan"
+          countNumber    = 10
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
