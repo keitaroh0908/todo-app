92ea2cf5750000672f498080a840b32a3de4e643
Keitaro Hayashimoto <keitaroh@haya-sys.com>
2024-10-09 11:47:36 +0900

    キャパシティのスケーリングを設定する

--- terraform/services/dynamodb/main.tf
+++ terraform/services/dynamodb/main.tf
@@ -3,8 +3,8 @@ resource "aws_dynamodb_table" "this" {
   range_key      = "taskId"
   name           = "tasks"
   billing_mode   = "PROVISIONED"
-  read_capacity  = 1
-  write_capacity = 1
+  read_capacity  = 5
+  write_capacity = 5
 
   attribute {
     name = "userId"
@@ -14,5 +14,53 @@ resource "aws_dynamodb_table" "this" {
   attribute {
     name = "taskId"
     type = "S"
+  }
+}
+
+resource "aws_appautoscaling_target" "read_capacity_units" {
+  service_namespace  = "dynamodb"
+  resource_id        = "table/${aws_dynamodb_table.this.name}"
+  scalable_dimension = "dynamodb:table:ReadCapacityUnits"
+  min_capacity       = 5
+  max_capacity       = 10
+}
+
+resource "aws_appautoscaling_policy" "read_capacity_units" {
+  name               = "DynamoDBReadCapacityUtilization:${aws_appautoscaling_target.read_capacity_units.resource_id}"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.read_capacity_units.resource_id
+  scalable_dimension = aws_appautoscaling_target.read_capacity_units.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.read_capacity_units.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "DynamoDBReadCapacityUtilization"
+    }
+
+    target_value = 70
+  }
+}
+
+resource "aws_appautoscaling_target" "write_capacity_units" {
+  service_namespace  = "dynamodb"
+  resource_id        = "table/${aws_dynamodb_table.this.name}"
+  scalable_dimension = "dynamodb:table:WriteCapacityUnits"
+  min_capacity       = 5
+  max_capacity       = 10
+}
+
+resource "aws_appautoscaling_policy" "write_capacity_units" {
+  name               = "DynamoDBWriteCapacityUtilization:${aws_appautoscaling_target.write_capacity_units.resource_id}"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.write_capacity_units.resource_id
+  scalable_dimension = aws_appautoscaling_target.write_capacity_units.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.write_capacity_units.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "DynamoDBWriteCapacityUtilization"
+    }
+
+    target_value = 70
   }
 }
